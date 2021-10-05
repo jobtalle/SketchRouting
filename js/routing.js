0fa6af6ed265212a09e7b197ab219b0689678444
Job <jobtalle@hotmail.com>
2021-10-05 10:27:19 +0200

    Created hex grid

--- js/routing.js
+++ js/routing.js
@@ -1,27 +1,15 @@
-import {Node} from "./node.js";
+import {Grid} from "./grid.js";
 
 export class Routing {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.grid = new Grid(width, height);
         this.background = document.createElement("canvas");
         this.background.width = width;
         this.background.height = height;
-        this.nodes = this.createNodes();
 
-        const backgroundContext = this.background.getContext("2d");
-
-        for (let node = 0, nodes = this.nodes.length; node < nodes; ++node)
-            this.nodes[node].draw(backgroundContext);
-    }
-
-    createNodes() {
-        const nodes = [];
-
-        for (let i = 0; i < 1000; ++i)
-            nodes.push(new Node(Math.random() * this.width, Math.random() * this.height));
-
-        return nodes;
+        this.grid.draw(this.background.getContext("2d"))
     }
 
     draw(context) {
